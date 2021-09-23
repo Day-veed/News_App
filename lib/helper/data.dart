61db0fc39a5d87e584626c18c6dec828552e841f
Day-veed <davidadekunle9@gmail.com>
2021-09-23 23:55:07 +0100

    update

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -22,7 +22,7 @@ List<CategoryModel> getCategories(){
   categoryModel.imageUrl = "https://images.unsplash.com/photo-1494059980473-813e73ee784b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80";
   category.add(categoryModel);
 
-  //4
+  ///4
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Sports";
   categoryModel.imageUrl = "https://images.unsplash.com/photo-1517649763962-0c623066013b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80";
